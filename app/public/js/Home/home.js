ef4f8a915d7c4d6654f171afc8a0504c35f8037b
Kaic Melo <kaicmelosantos@gmail.com>
2020-09-08 17:52:28 -0300

    module Format date

--- app/public/js/Home/home.js
+++ app/public/js/Home/home.js
@@ -15,13 +15,13 @@ $(document).ready(function() {
                 render:  data => {
                     if(data != null)
                     {
-                    return moment(data).format('DD/MM/YYYY');
+                    return dateFormat(data,'date-front');
                     }else{return null}
                 }
             }, 
             { 
                 data: 'created_at',
-                render:  data => moment(data).format('DD/MM/YYYY')
+                render:  data => dateFormat(data,'date-front')
             }, 
             { 
                 data: 'id',
@@ -54,86 +54,112 @@ $(document).ready(function() {
 
     $("#table_id tbody").on( 'click', 'button', function () {
         var dataRow = table.row( $(this).parents('tr') ).data();
-        console.log(dataRow);
-        $("#goal-date-concluded").val('');
 
+        console.log(dataRow);
+        console.log(moment(dataRow.created_at).format('YYYY-MM-DD'));
+        console.log(moment(dataRow.concluded).format('YYYY-MM-DD'));
+        
+        $("#goal-date-concluded").val('');
+        
+        $("#goal-id").val(dataRow.id);
         $("#goal-title").html(dataRow.name);
         $("#goal-create-by").val(dataRow.rk_user_id);
-        $("#goal-create-date").val(moment(dataRow.created_at).format('YYYY-MM-DD'));
+        $("#goal-create-date").val(dateFormat(dataRow.created_at,'date-front'));
         $("#goal-observation").val(dataRow.obs);
 
         if(dataRow.concluded)
         {
-            $("#goal-date-concluded").val(moment(dataRow.concluded).format('YYYY-MM-DD'));
+            $("#goal-date-concluded").val(dateFormat(dataRow.concluded),'date-back');
         }
 
-        $("#modal-save-goals").click(function(avent){
-            $.ajax({
-                url: "/updateGoals", 
-                method: "POST",
-                data: {
-                    id : dataRow.id,
-                    obs : $("#goal-observation").val(),
-                    concluded : $("#goal-date-concluded").val()
-                } ,
-                dataType: "json",
-                beforeSend : function(data){
-                    $('#exampleModal').modal('hide');
-                    $('#table_id').DataTable().ajax.reload();
-
-                    swal({
-                        title: "Meta atualizada com sucesso !",
-                        icon: "success",
-                        button: "Continuar",
-                    });
-                }
-            });
-        });
-
+        
         $("#data-concluded-clear").click(function(avent){
             $("#goal-date-concluded").val('');
         });
-
-        $("#delete-force-goal").click(function(avent){
-            swal({
-                title: "Tem certeza ?",
-                text: "Cuidado, sua meta será deletada",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            }).then((willDelete) => {
-                if (willDelete) {
-                    
-                    $.ajax({
-                        url: "/deleteGoal", 
-                        method: "POST",
-                        data: {id : dataRow.id} ,
-                        dataType: "text" 
-                    }).done(function(data) {
-                        $('#exampleModal').modal('hide');
-                        $('#confirm').modal('hide');
-                        $('#table_id').DataTable().ajax.reload();
-
-                        swal({
-                            title: "Registro apagado!",
-                            icon: "success",
-                            button: "Continuar",
-                        });
-
-                    }).fail(function() { 
-                        swal({
-                            title: "Houve um erro!",
-                            icon: "error",
-                            text: "Por favor, tente novamente",
-                            button: "Continuar",
-                        });
-                    });
-                } else {
-                    swal("Ufaa! Sua meta continuar de pé");
-                }
-            });
-        });
+        
     });  
+    
+    $("#modal-save-goals").click(function(avent){
+        $.ajax({
+            url: "/updateGoals", 
+            method: "POST",
+            dataType: "text",
+            data: {
+                id : $("#goal-id").val(),
+                obs : $("#goal-observation").val(),
+                concluded : $("#goal-date-concluded").val()
+            } ,
+            beforeSend : function(data){
+                $('#exampleModal').modal('hide');
+                swal({
+                    title: "Meta atualizada com sucesso !",
+                    icon: "success",
+                    button: "Continuar",
+                });
+            }
+        });
+
+        $('#table_id').DataTable().ajax.reload();
+    });
+
+    $("#delete-force-goal").click(function(avent){
+        swal({
+            title: "Tem certeza ?",
+            text: "Cuidado, sua meta será deletada",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                
+                $.ajax({
+                    url: "/deleteGoal", 
+                    method: "POST",
+                    data: {id : $("#goal-id").val()} ,
+                    dataType: "text" 
+                }).done(function(data) {
+                    $('#exampleModal').modal('hide');
+                    $('#confirm').modal('hide');
+                    $('#table_id').DataTable().ajax.reload();
+
+                    swal({
+                        title: "Registro apagado!",
+                        icon: "success",
+                        button: "Continuar",
+                    });
+
+                }).fail(function() { 
+                    swal({
+                        title: "Houve um erro!",
+                        icon: "error",
+                        text: "Por favor, tente novamente",
+                        button: "Continuar",
+                    });
+                });
+            } else {
+                swal("Ufaa! Sua meta continuar de pé");
+            }
+        });
+    });
+
+    function dateFormat(date,arg=null)
+    {   
+        switch(arg)
+        {
+            case 'date-front':
+                return moment(date).format('DD/MM/YYYY');
+            break;
+            case 'date-back':
+                return moment(date).format('YYYY-MM-DD');
+            break;
+            case 'dateTime':
+                return moment(date).format('YYYY-MM-DDTHH:mm:ss');
+            break;
+            default:
+                return moment(date).format('YYYY-MM-DD');
+                break;
+        }
+    }
 
     String.prototype.capitalize = function() {
         return this.charAt(0).toUpperCase() + this.slice(1);
