330e8ee8d797902dff247f4126efeeede0b5329f
KaicMelo <kakakaic@yahoo.com.br>
2020-12-19 05:48:22 -0300

    Resolving problem about my id in session

--- app/models/NotificationDAO.js
+++ app/models/NotificationDAO.js
@@ -4,7 +4,7 @@ function UsersDAO(connection) {
 }
 UsersDAO.prototype.getMyNotification = function (id, result) {
 
-    this._connection.query("SELECT * FROM rk_notification WHERE rk_user_id = 1 OR rk_girlfriend_id = 1 AND status = 1", result);
+    this._connection.query("SELECT * FROM rk_notification WHERE rk_user_id = "+id+" OR rk_girlfriend_id = "+id+" AND status = 1", result);
 }
 
 module.exports = function () {
